afb950e9ee2c42c3eb46d1e2c741f1df2cce8836
andrearanica <andrearanica2004@gmail.com>
2023-03-01 20:27:19 +0100

    💄 Fix UI

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Web app to simulate a fake blockchain made 100% in JavaScript.
 - Login and registration
 - Show all blocks
 - Mine new blocks
+
+### ❤️ Credits 
+<a href="https://www.flaticon.com/free-icons/more" title="more icons">More icons created by Freepik - Flaticon</a>
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import add from '../images/add-icon.png'
+
 export function Dashboard () {
     
     const [blockchain, setBlockchain] = useState([])
@@ -12,7 +14,7 @@ export function Dashboard () {
                 Authorization: `${ window.localStorage.getItem('token') }`
             }
         })
-        .then(res => setBlockchain(res.data))
+        .then(res => { setBlockchain(res.data); console.log(blockchain) })
     }, [blockchain])
 
     function newBlock (e) {
@@ -29,17 +31,21 @@ export function Dashboard () {
     return (
         <div className="container my-5 text-center">
             <h4>Blockchain</h4>
+            <center><img src={ add } onClick={ e => newBlock(e) } className="btn mx-4" id="button" /><img src={ add } onClick={ e => newBlock(e) } className="btn mx-4" id="button" /></center>
             <div className="row">
                 { 
                     blockchain.length > 0 ? 
                     blockchain.map(block => {
                         return ( 
-                        <div className="card col-lg my-2" style={{ width: "18rem", margin: 'auto' }}>
-                            <div className="card-body">
-                                <h5 className="card-title">Hash: { block.hash }</h5>
-                                <p className="card-text">Data: { block.data }</p>
-                                <p>Previous hash: { block.previousHash }</p>
-                                <p className="btn btn-primary">Tutte le info</p>
+                        <div className="col">
+                            <div className="card my-2" style={{ width: "18rem", margin: 'auto' }}>
+                                <div className="card-body">
+                                    <h5 className="card-title">Hash</h5>
+                                    <p className="card-text">{ block.hash }</p>
+                                    <p className="card-text">Data: { block.data }</p>
+                                    <p>Previous hash: { block.previousHash }</p>
+                                    <p className="btn btn-primary">Tutte le info</p>
+                                </div>
                             </div>
                         </div>
                         )
@@ -48,7 +54,6 @@ export function Dashboard () {
                 }
             </div>
             <input onChange={ e => { setNewBlockData(e.target.value); console.log(e.target.value) } } className="form-control my-2" />
-            <button onClick={ e => newBlock(e) } className="btn">Nuovo blocco</button>
         </div>
     )
 
